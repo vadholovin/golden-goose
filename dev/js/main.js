471b56fc278eb7b6b67e452a40dc45314b002207
Vadym Holovin <holyvad@gmail.com>
2019-08-01 14:18:22 +0300

    Styled header

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function () {
     svg4everybody({});
 });
+
+
+/**
+ * Menu
+ */
+$(document).ready(function () {
+  let toggleMenu = function () {
+    $('.hamburger').click(function (e) { 
+      e.preventDefault();
+      $(this).toggleClass('is-active');
+      $('.mobile-nav').toggleClass('is-active');
+    });
+  
+    $('.mobile-nav a').click(function () {
+      $('.mobile-nav').removeClass('is-active');
+      $('.hamburger').removeClass('is-active');
+    });
+  };
+
+  toggleMenu();
+});
