e036069d7d7754d31d0ddbc89bf4e1f50ccb1f77
Vadym Holovin <holyvad@gmail.com>
2019-08-05 10:09:37 +0300

    Added animation on scroll, mail script

--- dev/js/main.js
+++ dev/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
 
 /**
- * Menu
+ * #Menu
  */
 $(document).ready(function () {
   $('.hamburger').click(function (e) { 
@@ -21,7 +21,7 @@ $(document).ready(function () {
 
 
 /**
- * Slider
+ * #Slider
  */
 $(document).ready(function () {
   var slickElement = $('.js-hero-slider');
@@ -52,7 +52,7 @@ $(document).ready(function () {
 
 
 /**
- * Datepicker
+ * #Datepicker
  */
 $(document).ready(function () {
   $('#checkin, #checkout').datepicker({
@@ -62,31 +62,123 @@ $(document).ready(function () {
 
 
 /**
- * Smooth scroll to anchor
+ * #Smooth scroll to anchor
  */
 (function(){
-
   var scroll = new SmoothScroll('a[href*="#"]', {
     offset: 50,
   });
-
 })();
 
-/*------------------------------------*\
-  #Scroll Navbar
-\*------------------------------------*/
 
+/**
+ * #Micromodal init
+ */
+(function () {
+  MicroModal.init();
+})();
+
+
+/**
+ * #Animate on scroll
+ */
+(function () {
+  AOS.init({
+    once: true,
+    duration: 1000,
+    easing: 'ease-in-out',
+  });
+})();
+
+
+/**
+ * #Scroll hamburger
+ */
 (function($) {
 
   $(window).scroll(function(){	
-    "use strict";	
+    'use strict';	
     var b = $(window).scrollTop();
     
     if( b > 50) {		
-      $(".hamburger").addClass("hamburger--scroll");
+      $('.hamburger').addClass('hamburger--scroll');
     } else {
-      $(".hamburger").removeClass("hamburger--scroll");
+      $('.hamburger').removeClass('hamburger--scroll');
     }
   });
   
 })( jQuery );
+
+
+/**
+ * #Form validation
+ */
+$(document).ready(function () {
+
+  $('#formFeedback').submit(function(e){
+    e.preventDefault();
+
+    var form = $('#formFeedback');
+
+    form.validate({
+      rules: {
+        'user-name': 'required',
+        'user-email': 'required',
+        'user-message': 'required',
+      },
+      messages: {
+        'user-name': 'Ошибка',
+        'user-email': 'Ошибка',
+        'user-message': 'Ошибка',
+      },
+    });
+
+    if(form.valid()) {
+      $.ajax({
+        url : 'feedback.php',
+        type: 'POST',
+        data: $(this).serialize(),
+      }).done(function(res) {
+        answer(res);
+      });
+    }
+  });
+
+
+  // Answers
+  function answer(res) {
+    var answerTitle = document.querySelector('.modal--answer .answer-title');
+    var answerText = document.querySelector('.modal--answer .answer-text');
+
+    var positiveResp = {
+      title: 'Спасибо!',
+      text: 'Мы скоро свяжемся с Вами'
+    };
+
+    var negativeResp = {
+      title: 'Произошла ошибка!',
+      text: 'Попробуйте позже'
+    };
+
+    function hideAnswer() {
+      setTimeout(function() {
+        MicroModal.close('modal-answer');
+        answerTitle.textContent = "";
+        answerText.textContent = "";
+      }, 3000);
+    }
+
+    if(res == "error"){
+      answerTitle.textContent = negativeResp.title;
+      answerText.textContent = negativeResp.text;
+      MicroModal.show('modal-answer');
+      hideAnswer();
+    }
+    if(res == "done"){
+      answerTitle.textContent = positiveResp.title;
+      answerText.textContent = positiveResp.text;
+      MicroModal.show('modal-answer');
+      hideAnswer();
+    }
+  }
+});
